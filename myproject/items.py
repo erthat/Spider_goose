e8bec7c3c9f812509ee780334000968a900efd16
erthat <exdevil92@gmail.com>
2024-09-09 11:30:45 +0500

    Новый способ парсинга

--- myproject/items.py
+++ myproject/items.py
@@ -5,8 +5,7 @@
 
 import scrapy
 
-
-class MyprojectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MyItem(scrapy.Item):
+    title = scrapy.Field()
+    content = scrapy.Field()
+    date = scrapy.Field()
--- myproject/settings.py
+++ myproject/settings.py
@@ -102,11 +102,11 @@ REACTOR_THREADPOOL_MAXSIZE = 20
 RETRY_ENABLED = True
 RETRY_TIMES = 3
 
-# AUTOTHROTTLE_ENABLED = True
-# AUTOTHROTTLE_START_DELAY = 0.1  # начальная задержка между запросами в секундах
-# AUTOTHROTTLE_MAX_DELAY = 5  # максимальная задержка между запросами в секундах
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0  # ожидаемое количество одновременных запросов к каждому серверу
-# AUTOTHROTTLE_DEBUG = True
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 0.1  # начальная задержка между запросами в секундах
+AUTOTHROTTLE_MAX_DELAY = 5  # максимальная задержка между запросами в секундах
+AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0  # ожидаемое количество одновременных запросов к каждому серверу
+AUTOTHROTTLE_DEBUG = True
 # DOWNLOAD_DELAY = 10
 # В settings.py
 
